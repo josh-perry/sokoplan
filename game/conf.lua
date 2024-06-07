bb7bf537cb69f9045918fea0e3a19debe2999d4b
Josh Perry <joshperry@outlook.com>
2024-06-07 17:44:14 +0100

    Change love version to 12.0

--- game/conf.lua
+++ game/conf.lua
@@ -11,11 +11,12 @@ end
 function love.conf(t)
 	t.identity              = nil
 	t.appendidentity        = false
-	t.version               = "11.4"
+	t.version               = "12.0"
 	t.console               = false
 	t.accelerometerjoystick = false
 	t.externalstorage       = false
 	t.gammacorrect          = false
+	t.highdpi			    = false
 
 	t.audio.mic             = false
 	t.audio.mixwithsystem   = true
@@ -34,8 +35,7 @@ function love.conf(t)
 	t.window.msaa           = 0
 	t.window.depth          = nil
 	t.window.stencil        = nil
-	t.window.display        = 1
-	t.window.highdpi        = false
+	t.window.displayIndex   = 1
 	t.window.usedpiscale    = true
 	t.window.x              = nil
 	t.window.y              = nil
